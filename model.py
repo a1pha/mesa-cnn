359144721449059fd848a79ac5b19c86dd7bc0ca
Abhi Jadhav <aj246@duke.edu>
2018-07-20 12:43:39 -0400

    Updated models

--- model.py
+++ model.py
@@ -1,18 +1,53 @@
-import pandas as pd
+from __future__ import print_function
+import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from data_processing import filter_activity, get_activity
-import tensorflow as tf
 import pickle
 
-# Batch Size
+
+def process_key(key_np_array):
+    keys_processed = np.empty([key_np_array.size, 14400])
+    i = 0
+    for patient in key_np_array:
+        keys_processed[i] = filter_activity(get_activity(patient))
+        i += 1
+    return keys_processed
+
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+
+def conv1d(x, W):
+    return tf.nn.conv1d(x, W, stride=1, padding='SAME')
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='VALID')
+
+
+def maxpool1d(x):
+    return tf.nn.pool(x, [5], 'MAX', 'SAME', strides=[5])
+
+
+def maxpool2d(x):
+    return tf.nn.max_pool(x, ksize=[1, 5, 5, 1], strides=[1, 5, 5, 1], padding='SAME')
+
+# Initial Variables
 batch_size = 50
-n_classes = 1
+epochs = 3
 
 # Loading the outcome dataset and processing mesa-ids
 with open('elements_file.pickle', 'rb') as handle:
     elements_file = pickle.load(handle)
-with open('keys_file.pickle', 'rb') as handle:
+with open('keys_unprocessed_file.pickle', 'rb') as handle:
     keys_file = pickle.load(handle)
 
 
@@ -20,74 +55,68 @@ with open('keys_file.pickle', 'rb') as handle:
 X_train, X_test, y_train, y_test = train_test_split(keys_file, elements_file, test_size=0.20, random_state=42)
 
 
-# Model Parameters
-x = tf.placeholder('float', [None, 5*2880])
-y = tf.placeholder('float')
+# Input layer
+x = tf.placeholder(tf.float32, [None, 14400], name='x')
+y_ = tf.placeholder(tf.float32, [None, 1],  name='y_')
+x_image = tf.reshape(x, shape=[-1, 14400, 1])
 
-def conv1d(x, W):
-    return tf.nn.conv1d(x, W, stride=1, padding='SAME')
+# Convolutional layer 1
+W_conv1 = weight_variable([10, 1, 10])
+b_conv1 = bias_variable([10])
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+h_conv1 = tf.nn.relu(conv1d(x_image, W_conv1) + b_conv1)
+h_pool1 = maxpool1d(h_conv1)
+h_pool1 = tf.reshape(h_pool1, shape=[-1, 576, 5, 10])
 
-def maxpool1d(x):
-    return tf.nn.pool(x, [5], 'MAX', 'SAME', strides=[5])
+# Convolutional layer 2
+W_conv2 = weight_variable([5, 5, 10, 20])
+b_conv2 = bias_variable([20])
 
-def maxpool2d(x):
-    return tf.nn.max_pool(x, ksize=[1, 5, 5, 1], strides=[1, 5, 5, 1], padding='SAME')
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = maxpool2d(h_conv2)
 
-def convolutional_neural_network(x):
-    weights = {'W_conv1': tf.Variable(tf.random_normal([10, 1, 10])),
-               'W_conv2': tf.Variable(tf.random_normal([5, 5, 10, 20])),
-               'W_fc': tf.Variable(tf.random_normal([116000, 1024])),
-               'out': tf.Variable(tf.random_normal([1024, n_classes]))}
+# Fully connected layer
+h_pool2_flat = tf.reshape(h_pool2, [-1, 2300])
+W_fc1 = weight_variable([2300, 1024])
+b_fc1 = bias_variable([1024])
 
-    biases = {'b_conv1': tf.Variable(tf.random_normal([10])),
-              'b_conv2': tf.Variable(tf.random_normal([20])),
-              'b_fc': tf.Variable(tf.random_normal([1024])),
-              'out': tf.Variable(tf.random_normal([n_classes]))}
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    x = tf.reshape(x, shape=[-1, 14400, 1])
-    conv1 = conv1d(x, weights['W_conv1']) + biases['b_conv1']
-    conv1 = maxpool1d(conv1)
+# Dropout
+keep_prob  = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-    conv1 = tf.reshape(conv1, shape=[-1, 576, 5, 10])
-    conv2 = conv2d(conv1, weights['W_conv2']) + biases['b_conv2']
-    conv2 = maxpool2d(conv2)
+# Output Layer
+W_fc2 = weight_variable([1024, 1])
+b_fc2 = bias_variable([1])
 
-    fc = tf.reshape(conv2, shape=[-1, 116000])
-    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']+biases['b_fc']))
-
-    output = tf.matmul(fc, weights['out']+biases['out'])
-    return output
+y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='y')
 
 
-def train_neural_network(x):
-    prediction = convolutional_neural_network(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
+# Evaluation functions
+loss=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=y_))
+# Training algorithm
+optimizer = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
-    hm_epochs = 10
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
-
-            i = 0
-            while i < len(X_train):
-                start = i
-                end = i + batch_size
-                batch_x = np.array(X_train[start:end])
-                batch_y = np.array(y_train[start:end])
-                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
-                epoch_loss += c
-                i += batch_size
-
-            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
-
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x: X_test, y: y_test}))
-
-
-train_neural_network(x)
+# Training steps
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    indices = np.arange(1, X_train.size)
+    for epoch in range(epochs):
+        indices = shuffle(indices)
+        epoch_loss = 0
+        i = 0
+        while i < (len(X_train) - batch_size):
+            batch = np.random.choice(indices, size=batch_size)
+            batch_x = process_key(np.take(X_train, batch))
+            batch_y = np.take(y_train, batch)
+            batch_y = np.reshape(batch_y, (batch_y.size, 1))
+            merge = tf.summary.merge_all()
+            _, c = sess.run([optimizer, loss], feed_dict={x: batch_x, y_: batch_y, keep_prob: 1.0})
+            epoch_loss += c
+            i += batch_size
+            print(str(i) + " Samples Processed")
+        print('Epoch', epoch+1, 'completed out of', epochs, 'loss:', epoch_loss)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+    print('Accuracy:', accuracy.eval({x: process_key(X_test), y_: y_test, keep_prob: 1.0}))
--- model_keras.py
+++ model_keras.py
@@ -1,17 +1,14 @@
-from __future__ import print_function
 import keras
-from keras.datasets import mnist
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D, Conv1D, MaxPooling1D, Reshape, Dropout
 from keras.models import Sequential
 import matplotlib.pylab as plt
 from sklearn.model_selection import train_test_split
 import pickle
-from keras.callbacks import TensorBoard
 
-batch_size = 100
-num_classes = 2
-epochs = 1
+batch_size = 50
+num_classes = 1
+epochs = 3
 
 # input image dimensions
 img_x = 14400
@@ -23,7 +20,7 @@ with open('keys_file.pickle', 'rb') as handle:
     keys_file = pickle.load(handle)
 
 # Splitting the data into training and test
-X_train, X_test, y_train, y_test = train_test_split(keys_file, elements_file, test_size=0.20, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(keys_file, elements_file, test_size=0.20)
 # reshape the data into a 4D tensor - (sample_number, x_img_size, y_img_size, num_channels)
 # because the MNIST is greyscale, we only have a single channel - RGB colour images would have 3
 X_train = X_train.reshape(X_train.shape[0], img_x, 1)
@@ -38,9 +35,7 @@ print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices - this is for use in the
-# categorical_crossentropy loss below
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+
 
 
 model = Sequential()
@@ -49,7 +44,7 @@ model.add(Conv1D(10, kernel_size=10, strides=1,
                  input_shape=input_shape, padding='same'))
 model.add(MaxPooling1D(pool_size=5, strides=5))
 model.add(Reshape((576, 5, 10)))
-model.add(Conv2D(20, (5, 5), activation='sigmoid', padding='same'))
+model.add(Conv2D(20, (5, 5), activation='sigmoid', padding='valid'))
 model.add(MaxPooling2D(pool_size=(5, 1)))
 model.add(Dropout(0.25))
 model.add(Flatten())
@@ -57,7 +52,7 @@ model.add(Dense(1000, activation='sigmoid'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='sigmoid'))
 
-model.compile(loss=keras.losses.binary_crossentropy,
+model.compile(loss='binary_crossentropy',
               optimizer=keras.optimizers.SGD(lr=0.1, momentum=0.0, decay=1e-4, nesterov=False),
               metrics=['accuracy'])
 
@@ -79,7 +74,7 @@ model.fit(X_train, y_train,
           validation_data=(X_test, y_test),
           callbacks=[history],
           shuffle=True,)
-score = model.evaluate(X_test, y_test, verbose=0)
+score = model.evaluate(X_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 plt.plot(range(1, (epochs+1)), history.acc)
